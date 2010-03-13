967af91e85e0692f893205ebd76e687fa75a2e7f
tgarlanger <tgarlanger@33366f0e-2e5f-11df-8474-0f70896a4c6b>
2010-03-13 05:35:08 +0000

    Initial Commit!

---
+++ trunk/SantellosEscape/GameStateTest/Screens/CreditsScreen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameStateTest.Screens
+{
+    class CreditsScreen : Screen
+    {
+        private List<string> m_strCreditItems;
+
+        public CreditsScreen(List<string> strCreditItems, string strFontPath)
+        {
+            m_strCreditItems = strCreditItems;
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void LoadContent(Microsoft.Xna.Framework.Content.ContentManager Content, Microsoft.Xna.Framework.Graphics.SpriteBatch sprBatch)
+        {
+            base.LoadContent(Content, sprBatch);
+        }
+    }
+}
