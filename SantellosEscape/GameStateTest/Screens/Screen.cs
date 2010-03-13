ec7019645c8ee4b564fd2e03caf5695c0cc8d155
tgarlanger <tgarlanger@33366f0e-2e5f-11df-8474-0f70896a4c6b>
2010-03-13 20:11:58 +0000

    Fully commented Screen.cs

--- SantellosEscape/GameStateTest/Screens/Screen.cs
+++ SantellosEscape/GameStateTest/Screens/Screen.cs
@@ -7,22 +7,43 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 
+
 namespace GameStateTest.Screens
 {
+    /// <summary>
+    /// The possible states of a Screen
+    /// </summary>
+    /// <seealso cref="Screen"/>
     public enum ScreenState
     {
         Hidden = 0, Active
     }
 
+    /// <summary>
+    /// The possible types of a Screen
+    /// </summary>
+    /// <seealso cref="Screen"/>
     public enum ScreenType
     {
         Menu, Credits, Game
     }
 
+    /// <summary>
+    /// A base Screen class
+    /// 
+    /// All other Screen types are derived from this class
+    /// </summary>
     public abstract class Screen
     {
+        /// <summary>
+        /// The State of the Screen.
+        /// </summary>
         private ScreenState m_screenState;
 
+        /// <summary>
+        /// Gets or sets the state of the Screen.
+        /// </summary>
+        /// <value>The state of the Screen.</value>
         public ScreenState ScreenState
         {
             get
@@ -35,8 +56,15 @@ namespace GameStateTest.Screens
             }
         }
 
+        /// <summary>
+        /// The Type of the Screen.
+        /// </summary>
         private ScreenType m_screenType;
 
+        /// <summary>
+        /// Gets or sets the type of the Screen
+        /// </summary>
+        /// <value>The type of the Screen.</value>
         public ScreenType ScreenType
         {
             get
@@ -49,8 +77,15 @@ namespace GameStateTest.Screens
             }
         }
 
+        /// <summary>
+        /// The name of the Screen
+        /// </summary>
         private string m_strName;
 
+        /// <summary>
+        /// Gets or sets the name of the Screen.
+        /// </summary>
+        /// <value>The name of the Screen.</value>
         public string Name
         {
             get
@@ -63,28 +98,53 @@ namespace GameStateTest.Screens
             }
         }
 
+        /// <summary>
+        /// The SpriteBatch to draw to
+        /// </summary>
         protected SpriteBatch m_sprBatch;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Screen"/> class.
+        /// </summary>
         public Screen()
         {
         }
 
+        /// <summary>
+        /// Initializes this instance.
+        /// </summary>
         public virtual void Initialize()
         {
         }
 
+        /// <summary>
+        /// Loads the content.
+        /// </summary>
+        /// <param name="Content">The content to load from.</param>
+        /// <param name="sprBatch">The SpriteBatch to draw to.</param>
         public virtual void LoadContent(ContentManager Content, SpriteBatch sprBatch)
         {
         }
 
+        /// <summary>
+        /// Unloads the content.
+        /// </summary>
         public virtual void  UnloadContent()
         {
         }
 
+        /// <summary>
+        /// Updates the instance in respect to the specified game time.
+        /// </summary>
+        /// <param name="gameTime">The game time.</param>
         public virtual void  Update(Microsoft.Xna.Framework.GameTime gameTime)
         {
         }
 
+        /// <summary>
+        /// Draws the instance in respect to the specified game time.
+        /// </summary>
+        /// <param name="gameTime">The game time.</param>
         public virtual void  Draw(Microsoft.Xna.Framework.GameTime gameTime)
         {
         }
