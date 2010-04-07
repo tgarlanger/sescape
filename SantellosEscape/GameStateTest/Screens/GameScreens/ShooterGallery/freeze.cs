e0e16ce434a9ad137c7cf4ba50b7215308ac0990
tgarlanger <tgarlanger@33366f0e-2e5f-11df-8474-0f70896a4c6b>
2010-04-07 21:09:13 +0000

    Adding Freeze

---
+++ SantellosEscape/GameStateTest/Screens/GameScreens/ShooterGallery/freeze.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace SantellosEscape.Screens.GameScreens.ShooterGallery
+{
+    class freeze
+    {
+        Vector2 Position;
+        int GameTimer;
+        int Duration = 2;
+        
+
+        public bool DoubleScore(GameTime gameTime)
+        {
+            //if (called == true)
+            {
+                GameTimer = gameTime.TotalGameTime.Seconds + (gameTime.TotalGameTime.Minutes * 60);
+                if ((gameTime.TotalGameTime.Seconds + (gameTime.TotalGameTime.Minutes * 60)) - GameTimer >= Duration)
+                    return false;
+                else return true;
+            }
+        }
+
+
+        
+        
+    }
+}
